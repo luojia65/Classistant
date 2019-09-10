2b84fc2d9f35e7b23cadd4dd085381ddff2f4623
luojia65 <me@luojia.cc>
2019-09-10 19:57:33 +0800

    Update sql script

--- init.sql
+++ init.sql
@@ -8,24 +8,22 @@ CREATE TABLE `DUserAuth` (
 
 CREATE PROCEDURE `PUserRegister`(
 	IN `_nickname` VARCHAR(48),
-	IN `_hash` BLOB,
-  OUT `_return_id` INT,
-  OUT `_user_id` INT
+	IN `_hash` BLOB
 )
 BEGIN
 	DECLARE `_tmp_user_id` varchar(48);
+  DECLARE `_return_id` INT;
     
 	SELECT `user_id` 
-    INTO `_tmp_user_id`
-    FROM `DUserAuth` WHERE `nickname` = `_nickname`;
+  INTO `_tmp_user_id`
+  FROM `DUserAuth` WHERE `nickname` = `_nickname`;
     
-    IF `_tmp_user_id` IS NOT NULL THEN 
-		SET `_return_id` = 1; -- nickname already taken
+  IF `_tmp_user_id` IS NOT NULL THEN 
+    SELECT 1 as `return_id`; -- nickname already taken
 	ELSE
-		INSERT INTO `DUserAuth` (`nickname`,`hash`) 
-        VALUES (`_nickname`,`hash`);
-        SELECT `user_id` INTO `_tmp_user_id` FROM `DUserAuth` WHERE `nickname` = `_nickname`;
-        SET `_user_id` = `_tmp_user_id`;
-        SET `_return_id` = 0; -- success
-    END IF;
+    INSERT INTO `DUserAuth` (`nickname`,`hash`) 
+    VALUES (`_nickname`,`hash`);
+    SELECT `user_id` INTO `_tmp_user_id` FROM `DUserAuth` WHERE `nickname` = `_nickname`;
+    SELECT 0 as `return_id`, `_tmp_user_id` as `user_id`; -- success
+  END IF;
 END
