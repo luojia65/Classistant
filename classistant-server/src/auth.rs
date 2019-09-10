785cbcfdc4c5b692e6acd883ede5ca3c325112b7
luojia65 <me@luojia.cc>
2019-09-10 17:21:11 +0800

    Update

--- classistant-server/src/auth.rs
+++ classistant-server/src/auth.rs
@@ -1,48 +1,60 @@
 use actix_session::Session;
 use actix_web::{web, Responder, HttpRequest, HttpResponse};
-use rsa::{RSAPrivateKey, PublicKey, PaddingScheme};
-use rand::rngs::OsRng;
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
-struct PublicKeyStruct {
-    n: String,
-    e: String,
+const ACTION_REGISTER_REQUEST: &str = "v1.auth.register.request";
+const ACTION_REGISTER_REPLY: &str = "v1.auth.register.reply";
+
+#[derive(Deserialize)]
+pub struct RegisterRequest {
+    action: String,
+    nickname: String,
+    hash: String,
+    locale: String,
 }
 
-lazy_static! {
-    static ref AUTH_KEY: RSAPrivateKey = {
-        let mut rng = OsRng::new().expect("get secure randomness");
-        let bits = 2048;
-        RSAPrivateKey::new(&mut rng, bits).expect("generate a key")
+#[derive(Serialize)]
+pub struct RegisterResult {
+    action: &'static str,
+    success: bool,
+    #[serde(flatten)]
+    detailed: Detailed,
+}
+
+#[derive(Serialize)]
+pub enum Detailed {
+    Success { uid: String },
+    Failed { reason: String },
+}
+
+pub fn register(info: web::Json<RegisterRequest>) -> impl Responder {
+    let resp = RegisterResult {
+        action: ACTION_REGISTER_REPLY,
+        success: false,
+        detailed: Detailed::Failed { reason: String::from("滑稽，滑稽") }
     };
-    static ref AUTH_KEY_JSON: String = {
-        serde_json::to_string(&PublicKeyStruct {
-            n: base64::encode(&AUTH_KEY.n().to_bytes_le()),
-            e: base64::encode(&AUTH_KEY.e().to_bytes_le()),
-        }).expect("serialize as json")
+    let ans = match serde_json::to_string(&resp) {
+        Ok(r) => r,
+        Err(e) => {
+            let e_string = format!("{}", e).escape_unicode().to_string();
+            format!("{{\"action\":{},\"success\":false,\"reason\":\"{}\"}}", 
+                ACTION_REGISTER_REPLY.escape_unicode().to_string(),
+                e_string
+            )
+        }
     };
+    format!("{}", ans)
 }
 
-pub fn init(_session: Session) -> impl Responder {
-    format!("{}", *AUTH_KEY_JSON)
-}
+// #[derive(Serialize, Deserialize)]
+// struct ValidateResult {
+//     code: u32,
+//     description: &'static str
+// }
 
-#[derive(Serialize, Deserialize)]
-struct ValidateResult {
-    code: u32,
-    description: &'static str
-}
-
-pub fn validate(_session: Session) -> impl Responder {
-    // let a = AUTH_KEY.encrypt(&mut rand::thread_rng(), PaddingScheme::PKCS1v15, 
-    //     "huaji".as_bytes()
-    // ).unwrap();
-    // let b = AUTH_KEY.decrypt(PaddingScheme::PKCS1v15, &a).unwrap();
-    // println!("{:?}", b);
-    // decrypt password from request
-    HttpResponse::Ok().json(ValidateResult {
-        code: 0,
-        description: "Login succeeded"
-    })
-}
+// pub fn validate(_session: Session) -> impl Responder {
+//     HttpResponse::Ok().json(ValidateResult {
+//         code: 0,
+//         description: "Login succeeded"
+//     })
+// }
--- classistant-server/src/main.rs
+++ classistant-server/src/main.rs
@@ -1,6 +1,3 @@
-#[macro_use]
-extern crate lazy_static;
-
 use actix_web::{web, App, HttpServer};
 
 mod auth;
@@ -8,8 +5,7 @@ mod auth;
 fn main() {
     HttpServer::new(|| {
         App::new()
-            .route("/api/v1/auth/init", web::get().to(auth::init))
-            .route("/api/v1/auth/validate", web::get().to(auth::validate))
+            .route("/api/v1/auth/register", web::post().to(auth::register))
     })
     .bind("127.0.0.1:8000").expect("bind to port 8000")
     .run().expect("start server");
