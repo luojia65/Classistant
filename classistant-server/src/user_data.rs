ba8eead0cf8c54f61100d098da2a2ee0864704b6
luojia65 <me@luojia.cc>
2019-09-16 15:59:56 +0800

    Modify user data

--- classistant-server/src/user_data.rs
+++ classistant-server/src/user_data.rs
@@ -11,7 +11,27 @@ pub struct ModifyRequest {
     data: serde_json::Value,
 }
 
+#[derive(Serialize)]
+pub struct ModifyReply {
+    action: &'static str,
+    return_id: u32,
+}
+
 pub fn modify(db: web::Data<mysql::Pool>, info: web::Json<ModifyRequest>) -> HttpResponse {
+    if info.action != ACTION_MODIFY_REQUEST {
+        return register_failed(20, "wrong action type");
+    }
+    
     HttpResponse::Ok().json(&info.data)
+}
+
+#[inline]
+fn register_failed<T: Into<String>>(id: u32, reason: T) -> HttpResponse {
+    HttpResponse::Ok().json(ModifyReply {
+        action: ACTION_MODIFY_REPLY,
+        return_id: id,
+        // failed_reason: Some(reason.into()),
+        // success_uid: None,
+    })
 }
 
