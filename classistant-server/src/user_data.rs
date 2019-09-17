f04a590c8883c21aaec698a9bb6f9eb0b4f11ae3
luojia65 <me@luojia.cc>
2019-09-17 18:40:20 +0800

    Affected rows

--- classistant-server/src/user_data.rs
+++ classistant-server/src/user_data.rs
@@ -20,62 +20,72 @@ pub struct ModifyReply {
     #[serde(rename = "reason")]
     #[serde(skip_serializing_if = "Option::is_none")]
     failed_reason: Option<String>,
-    succeed: Vec<String>,
+    state: serde_json::Value,
 }
 
 pub fn modify(db: web::Data<mysql::Pool>, info: web::Json<ModifyRequest>) -> HttpResponse {
-    let mut succeed = Vec::new();
+    let mut state_map = serde_json::Map::new();
     if info.action != ACTION_MODIFY_REQUEST {
-        return modify_failed(20, "wrong action type", succeed);
+        return modify_failed(20, "wrong action type", state_map);
     }
     if info.uid == 0 {
-        return modify_failed(10, "invalid `uid`: cannot be zero", succeed);
+        return modify_failed(10, "invalid `uid`: cannot be zero", state_map);
     }
     let map = if let serde_json::Value::Object(map) = &info.data { 
         map
     } else {
-        return modify_failed(22, "invalid `data` field: object required", succeed);
+        return modify_failed(22, "invalid `data` field: object required", state_map);
     };
     let mut conn = match db.get_conn() {
         Ok(r) => r,
-        Err(_) => return modify_failed(30, "failed to get connection from database", succeed),    
+        Err(_) => return modify_failed(30, "failed to get connection from database", state_map),    
     };
     let mut stmt = match conn.prepare("PDataInsert(?, ?, ?, ?)") { 
         Ok(r) => r,
-        Err(_) => return modify_failed(31, "failed to prepare statement", succeed),    
+        Err(_) => return modify_failed(31, "failed to prepare statement", state_map),    
     };
     for (key, value) in map.iter() {
         let value = if let serde_json::Value::String(s) = value {
             s
         } else {
-            return modify_failed(23, "value in `data` must be a base64 encoded string", succeed);
+            return modify_failed(23, "value in `data` must be a base64 encoded string", state_map);
         };
         let value = match base64::decode(&value) {
             Ok(r) => r,
-            Err(_) => return modify_failed(21, "failed to decode base64 value", succeed),    
+            Err(_) => return modify_failed(21, "failed to decode base64 value", state_map),    
         };
         let type_id: [u8; 16] = md5::compute(key).into();
-        match stmt.execute((info.uid, info.gid, &type_id, value)) {
-            Ok(_) => {},
-            Err(_) => return modify_failed(32, "failed to execute statement", succeed),    
+        let result = match stmt.execute((info.uid, info.gid, &type_id, value)) {
+            Ok(r) => r,
+            Err(_) => return modify_failed(32, "failed to execute statement", state_map),    
         };
-        succeed.push(key.to_string());
+        let state = match result.affected_rows() {
+            0 /* Same value exists */ => 0,
+            1 /* Insert success */ => 1,
+            2 /* Update success */=> 2,
+            _ => -1,
+        };
+        state_map.insert(key.to_string(), serde_json::Value::Number(state.into()));
     }
     HttpResponse::Ok().json(ModifyReply {
         action: ACTION_MODIFY_REPLY,
         return_id: 0,
         failed_reason: None,
-        succeed
+        state: serde_json::Value::Object(state_map)
     })
 }
 
 #[inline]
-fn modify_failed<T: Into<String>>(id: u32, reason: T, succeed: Vec<String>) -> HttpResponse {
+fn modify_failed<T: Into<String>>(
+    id: u32, 
+    reason: T, 
+    state_map: serde_json::Map<String, serde_json::Value>
+) -> HttpResponse {
     HttpResponse::Ok().json(ModifyReply {
         action: ACTION_MODIFY_REPLY,
         return_id: id,
         failed_reason: Some(reason.into()),
-        succeed
+        state: serde_json::Value::Object(state_map)
     })
 }
 
