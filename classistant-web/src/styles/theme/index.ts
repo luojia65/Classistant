e01123a8ac56cdd493a222fe81a38a9840dd6b74
john Maxwell <iuiuiepi@outlook.com>
2019-10-18 00:01:41 +0800

    add css

--- classistant-web/src/styles/theme/index.ts
+++ classistant-web/src/styles/theme/index.ts
@@ -1,2 +1,17 @@
-export { default as dark } from './dark'
-export { default as light } from './light'
+import { createMuiTheme } from '@material-ui/core';
+
+import palette from './palette';
+import typography from './typography';
+import overrides from './overrides';
+
+const theme = createMuiTheme({
+  palette,
+  typography,
+  overrides,
+  zIndex: {
+    appBar: 1200,
+    drawer: 1100
+  }
+});
+
+export default theme;
