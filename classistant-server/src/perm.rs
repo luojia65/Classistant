a0722ddffe3d11f6fde3933ee7b56d16e17d255f
luojia65 <me@luojia.cc>
2020-02-05 20:43:43 +0800

    Save before bed

--- classistant-server/src/perm.rs
+++ classistant-server/src/perm.rs
@@ -11,3 +11,14 @@ pub fn check_perm_user(perm: &str, user_id: u64) -> crate::Result<bool> {
     let a: Perm = from_str(perm)?;
     Ok(a.groups.contains(&user_id))
 }
+
+
+pub fn check_perm_group(perm: &str, groups: Vec<u64>) -> crate::Result<bool> {
+    let a: Perm = from_str(perm)?;
+    for i in groups {
+        if a.groups.contains(&i) {
+            return Ok(true)
+        }
+    }
+    Ok(false)
+}
